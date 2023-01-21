00a5cbc9ba6382d5c02007771e79e6836cea318f
TiagoBBA <tiagobbandrade2704@gmail.com>
2023-01-21 18:34:14 -0300

    refactor: edit react router dom in main element

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar";
 
 export function App() {
-  return <Sidebar isActive="favorites" />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sidebar isActive="pokemons" />} />
+        <Route path="/favorites" element={<Sidebar isActive="favorites" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
