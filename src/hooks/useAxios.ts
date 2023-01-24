be522e07917560c441557fe620e72df5a931be0e
TiagoBBA <tiagobbandrade2704@gmail.com>
2023-01-24 03:25:12 -0300

    feat: create fetching custom hook

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { getPokemons, getSpecsFromPokemon } from "../services/api";
+import { SpecsFromPokemonType } from "../types/types";
+
+export function useAxios() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemons, setPokemons] = useState<SpecsFromPokemonType[]>();
+
+  async function fetchPokemons() {
+    const { pokemonsCompleteResponse } = await getPokemons();
+    const specsFromPokemonPromise = pokemonsCompleteResponse?.map((pokemon) =>
+      getSpecsFromPokemon(pokemon.url)
+    );
+
+    await Promise.all(specsFromPokemonPromise)
+      .then((response) => setPokemons(response))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return { pokemons, isLoading };
+}
