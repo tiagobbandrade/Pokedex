da0b98de6fd4f6cb676f530dabaee20de5bdecb5
Tiago Andrade <tiagobbandrade2704@gmail.com>
2024-11-01 14:32:39 -0300

    feat: create fixed slider

---
+++ src/components/containers/Slider.tsx
@@ -0,0 +1,38 @@
+export function Slider() {
+  return (
+    <div className="flex items-center flex-col gap-2.5 w-full overflow-hidden relative dark-fade-to-r dark-fade-to-l mt-[101px]">
+      <div className="flex items-center gap-14">
+        <span className="text-[#212227] text-2xl">Pikachu</span>
+        <span className="text-[#212227] text-2xl">Blastoise</span>
+        <span className="text-[#212227] text-2xl">Squirtle</span>
+        <span className="text-[#212227] text-2xl">Ivysaur</span>
+        <span className="text-[#212227] text-2xl">Charmander</span>
+        <span className="text-[#212227] text-2xl">Charmeleon</span>
+        <span className="text-[#212227] text-2xl">Raichu</span>
+        <span className="text-[#212227] text-2xl">Pikachu</span>
+        <span className="text-[#212227] text-2xl">Blastoise</span>
+        <span className="text-[#212227] text-2xl">Squirtle</span>
+        <span className="text-[#212227] text-2xl">Ivysaur</span>
+        <span className="text-[#212227] text-2xl">Charmander</span>
+        <span className="text-[#212227] text-2xl">Charmeleon</span>
+        <span className="text-[#212227] text-2xl">Raichu</span>
+      </div>
+      <div className="flex items-center ml-16 gap-14">
+        <span className="text-[#212227] text-2xl">Pikachu</span>
+        <span className="text-[#212227] text-2xl">Blastoise</span>
+        <span className="text-[#212227] text-2xl">Squirtle</span>
+        <span className="text-[#212227] text-2xl">Ivysaur</span>
+        <span className="text-[#212227] text-2xl">Charmander</span>
+        <span className="text-[#212227] text-2xl">Charmeleon</span>
+        <span className="text-[#212227] text-2xl">Raichu</span>
+        <span className="text-[#212227] text-2xl">Pikachu</span>
+        <span className="text-[#212227] text-2xl">Blastoise</span>
+        <span className="text-[#212227] text-2xl">Squirtle</span>
+        <span className="text-[#212227] text-2xl">Ivysaur</span>
+        <span className="text-[#212227] text-2xl">Charmander</span>
+        <span className="text-[#212227] text-2xl">Charmeleon</span>
+        <span className="text-[#212227] text-2xl">Raichu</span>
+      </div>
+    </div>
+  );
+}
