71317becc93d7a3823fe588842502b2b2e24e563
Tiago Andrade <tiagobbandrade2704@gmail.com>
2024-11-01 14:30:36 -0300

    feat: add inter font

--- src/App.tsx
+++
@@ -1,3 +0,0 @@
-export function App() {
-  return <h1>Pokedex</h1>;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
+import { Index } from "./pages/Index.tsx";
+
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Index />
+    </BrowserRouter>
   </StrictMode>
 );
