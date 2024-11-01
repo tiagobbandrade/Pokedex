b4a709e06b7195506826c3e8035f20201ba4974e
Tiago Andrade <tiagobbandrade2704@gmail.com>
2024-11-01 14:34:58 -0300

    feat: create a all pokemon badge

---
+++ src/components/ui/Switcher.tsx
@@ -0,0 +1,9 @@
+export function Switcher() {
+  return (
+    <div className="p-1.5 w-fit relative rounded-md bg-[#191B20] flex items-center gap-2 mx-auto mt-24">
+      <div className="text-[#191B20] px-4 py-[18px] rounded-[5px] text-base leading-none font-medium bg-[#33CC99] transition-all duration-300">
+        Todos pokémons
+      </div>
+    </div>
+  );
+}
